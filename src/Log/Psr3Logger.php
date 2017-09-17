aaf1d8943713e0a0f8ffade1c59f7927ebd25b3d
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2017-09-17 15:59:10 +1200

    Add Psr3Logger and LineFormatter

---
+++ src/Log/Psr3Logger.php
@@ -0,0 +1,110 @@
+<?php
+namespace ShoppinPal\YapepCommon\Log;
+
+use Psr\Log\LoggerInterface;
+use YapepBase\Config;
+use YapepBase\Log\LoggerAbstract;
+use YapepBase\Log\Message\IMessage;
+
+class Psr3Logger extends LoggerAbstract
+{
+
+    const APPLICATION_UNKNOWN = 'UNKNOWN';
+
+    /** @var string */
+    protected $programName;
+
+    /** @var string */
+    protected $applicationName;
+
+    /** @var LoggerInterface */
+    protected $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+
+        $this->programName = Config::getInstance()->get('system.project.name');
+    }
+
+    /**
+     * @return LoggerInterface
+     */
+    public function getPsr3Logger()
+    {
+        return $this->logger;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function logMessage(IMessage $message)
+    {
+        $context = [
+            'program'     => $this->programName,
+            'application' => $this->getApplicationName(),
+            'hostname'    => php_uname('n'),
+            'language'    => 'php',
+            'sapi'        => PHP_SAPI,
+            'tag'         => $message->getTag(),
+            'fields'      => $message->getFields(),
+        ];
+
+
+        switch ($message->getPriority()) {
+            case LOG_DEBUG:
+                $this->logger->debug($message->getMessage(), $context);
+                break;
+
+            case LOG_INFO:
+                $this->logger->info($message->getMessage(), $context);
+                break;
+
+            case LOG_NOTICE:
+                $this->logger->notice($message->getMessage(), $context);
+                break;
+
+            case LOG_WARNING:
+                $this->logger->warning($message->getMessage(), $context);
+                break;
+
+            case LOG_ERR:
+                $this->logger->error($message->getMessage(), $context);
+                break;
+
+            case LOG_CRIT:
+                $this->logger->critical($message->getMessage(), $context);
+                break;
+
+            case LOG_ALERT:
+                $this->logger->alert($message->getMessage(), $context);
+                break;
+
+            case LOG_EMERG:
+                $this->logger->emergency($message->getMessage(), $context);
+                break;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    protected function getApplicationName()
+    {
+        if (null !== $this->applicationName) {
+            return $this->applicationName;
+        }
+
+        $applicationName = Config::getInstance()->get('application.name', self::APPLICATION_UNKNOWN);
+
+        if (self::APPLICATION_UNKNOWN != $applicationName)
+        {
+            $this->applicationName = $applicationName;
+        }
+
+        return $applicationName;
+    }
+}
