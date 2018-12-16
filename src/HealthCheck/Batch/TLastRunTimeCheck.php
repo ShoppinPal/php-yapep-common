8d091891ef0cd5a1c9c43541e903f55616c33884
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2018-12-16 22:40:14 +1300

    Add parent directory creation to the health check file save

--- src/HealthCheck/Batch/TLastRunTimeCheck.php
+++ src/HealthCheck/Batch/TLastRunTimeCheck.php
@@ -5,6 +5,10 @@ trait TLastRunTimeCheck
 {
     protected function updateHealthCheckFile($path)
     {
+        if (!file_exists(basename($path))) {
+            mkdir(basename($path), 0755, true);
+        }
+
         file_put_contents($path, time(), LOCK_EX);
     }
 }
